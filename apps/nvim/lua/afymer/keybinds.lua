dfae02850c8898aa4b28ae647bb5ba1713b6e46e
Rémy Fayet <rems64des@gmail.com>
2025-07-27 10:13:44 +0200

    add navigation keymaps

--- apps/nvim/lua/afymer/keybinds.lua
+++ apps/nvim/lua/afymer/keybinds.lua
@@ -1,23 +1,29 @@
-vim.keymap.set("n", "<leader>f", ":Oil<CR>", { desc = "Open [F]ile explorer" })
-
-vim.keymap.set("n", "<leader-S>f", function()
-	require("conform").format({ async = true, lsp_format = "fallback" })
-end, { desc = "[F]ormat buffer" })
-
 vim.keymap.set(
-	"n",
-	"<C-Tab>",
-	":tabnext<CR>",
-	{ noremap = true, silent = true }
-)
-
-vim.keymap.set(
-	"n",
+	"",
 	"<C-S-Tab>",
 	":tabprevious<CR>",
 	{ noremap = true, silent = true }
 )
+vim.keymap.set("n", "<leader>f", ":Oil<CR>", { desc = "Open [F]ile explorer" })
+
+vim.keymap.set("", "<C-Tab>", ":tabnext<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<Tab>", ">>", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-Tab>", "<<", { noremap = true, silent = true })
+
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
+
+-- Move text
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-j>", "20j")
-
 vim.keymap.set("n", "<C-k>", "20k")
+
+vim.keymap.set("n", "<C-S-E>", "20<C-E>")
+vim.keymap.set("n", "<C-S-Y>", "20<C-Y>")
+
+vim.keymap.set("n", "<leader>l", function()
+	vim.lsp.buf.hover()
+end)
