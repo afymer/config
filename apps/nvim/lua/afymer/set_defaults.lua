9cf2b53b81b11b39c6878757dd5f2944c6a49234
Rémy Fayet <rems64des@gmail.com>
2025-07-27 10:14:18 +0200

    change window switch keybinds to prevent conflict

--- apps/nvim/lua/afymer/set_defaults.lua
+++ apps/nvim/lua/afymer/set_defaults.lua
@@ -115,25 +115,25 @@ vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set(
 	"n",
-	"<C-h>",
+	"<C-A-h>",
 	"<C-w><C-h>",
 	{ desc = "Move focus to the left window" }
 )
 vim.keymap.set(
 	"n",
-	"<C-l>",
+	"<C-A-l>",
 	"<C-w><C-l>",
 	{ desc = "Move focus to the right window" }
 )
 vim.keymap.set(
 	"n",
-	"<C-j>",
+	"<C-A-j>",
 	"<C-w><C-j>",
 	{ desc = "Move focus to the lower window" }
 )
 vim.keymap.set(
 	"n",
-	"<C-k>",
+	"<C-A-k>",
 	"<C-w><C-k>",
 	{ desc = "Move focus to the upper window" }
 )
